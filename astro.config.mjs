40bd7709cf85fa3cc40b95f5a1c4fd9900fe92d6
Siddhesh Thadeshwar <siddhesht@outlook.in>
2023-07-27 18:32:36 +0530

    Add github action workflow to deploy to pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://djsiddz.github.io',
+  base: '/books-for-devs'
+});

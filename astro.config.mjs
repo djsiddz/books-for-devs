70ec55c2c7e12d2df8e0e7610ac577a07ca6f91c
Siddhesh Thadeshwar <siddhesht@outlook.in>
2023-07-30 11:23:30 +0530

    Fix broken image links from public dir

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://djsiddz.github.io',
-  base: '/books-for-devs'
+  base: '/books-for-devs/',
+  publicDir: '/books-for-devs/public'
 });

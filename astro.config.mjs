306f6ce552fc997145089b100fe2e163ab8af954
Siddhesh Thadeshwar <siddhesht@outlook.in>
2023-08-16 22:29:07 +0530

    fix: Fix images not loading from dist

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://djsiddz.github.io',
-  base: '/books-for-devs/',
-  publicDir: '/books-for-devs/public'
+  base: '/books-for-devs'
 });

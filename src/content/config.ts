b423a6e005245b77044ea593559e2ce7822ca7d0
Siddhesh Thadeshwar <siddhesht@outlook.in>
2023-08-30 16:18:38 +0530

    feat(app): Create books content collection & add config schema using zod

---
+++ src/content/config.ts
@@ -0,0 +1,21 @@
+// 1. Import utilities from `astro:content`
+import { defineCollection, z } from 'astro:content';
+// 2. Define your collection(s)
+const booksCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    authorName: z.string(),
+    publisher: z.string(),
+    publicationDate: z.string().transform((str) => new Date(str)),
+    printLength: z.number(),
+    amazonINLink: z.string(),
+    coverImage: z.string(),
+    description: z.string()
+  })
+});
+// 3. Export a single `collections` object to register your collection(s)
+//    This key should match your collection directory name in "src/content"
+export const collections = {
+  'books': booksCollection,
+};

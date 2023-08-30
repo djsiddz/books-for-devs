561df46c8ca1aa4d6575391645733d89efa6fb9e
Siddhesh Thadeshwar <siddhesht@outlook.in>
2023-08-30 16:14:08 +0530

    Add yarnrc; Add astro mdx integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://djsiddz.github.io',
-  base: '/books-for-devs'
+  base: '/books-for-devs',
+  integrations: [mdx()]
 });

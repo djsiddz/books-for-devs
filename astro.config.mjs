24989f3389a4a8afade7c4aaccda3f5d94325bac
Siddhesh Thadeshwar <siddhesht@outlook.in>
2023-12-28 14:39:55 +0530

    Upgrade to Astro v4; Update Typescript & Yarn; Migration changes to Base URL; Closes #5

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://djsiddz.github.io',
-  base: '/books-for-devs',
+  base: '/books-for-devs/',
   integrations: [mdx()]
 });

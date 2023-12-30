13045bd6b3734ee0c291ae00f0db43e006acefa1
Siddhesh Thadeshwar <siddhesht@outlook.in>
2023-12-30 12:21:57 +0530

    feat(app): #4 Implement Recommendation & Reading Status tags for books
    Closes: #4

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,6 @@
 // 1. Import utilities from `astro:content`
 import { defineCollection, z } from 'astro:content';
+
 // 2. Define your collection(s)
 const booksCollection = defineCollection({
   type: "content",
@@ -12,7 +13,9 @@ const booksCollection = defineCollection({
     printLength: z.number(),
     amazonINLink: z.string(),
     coverImage: z.string(),
-    description: z.string()
+    description: z.string(),
+    readingStatus: z.enum(["Currently Reading 👓", "Read ✅", "Wishlisted ⭐", "Next Up ⏭️"]).optional(),
+    recommended: z.boolean().optional()
   })
 });
 // 3. Export a single `collections` object to register your collection(s)

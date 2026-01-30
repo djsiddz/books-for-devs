474bb3efe3664ad677ce6d3eb85be6bc7655c443
Siddhesh Thadeshwar <siddhesht@outlook.in>
2026-01-30 18:04:37 +0530

    switch to pnpm; update deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
-
+import tailwindcss from '@tailwindcss/vite';
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://djsiddz.github.io',
   base: '/books-for-devs/',
-  integrations: [mdx()]
+  integrations: [mdx()],
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
